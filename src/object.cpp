be42e4f444640d0dc4459e21d1eb979e26b8af56
Einar Johan T. Sømåen <einarjohants@gmail.com>
2012-02-03 04:05:27 +0100

    FITD: Add in most of the ResidualVM copyright-header
    The only thing missing is the GPLv2-or-later section, as I need
    to make sure that we are not GPLv2-only, osystemSDL_GL indicates that
    we might me or-later.

--- src/object.cpp
+++ src/object.cpp
@@ -1,3 +1,23 @@
+/* ResidualVM - A 3D game interpreter
+ *
+ * ResidualVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the AUTHORS
+ * file distributed with this source distribution.
+ 
+ <TODO: Add in GPLv2-notice, need to make sure if we are v2-only, or v2-or-later,
+ we are atleast v2>
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
 #include "common.h"
 
 int copyObjectToActor(int flag2, int var1, int foundName, short int flag, int x, int y, int z, int stage, int room, int alpha, int beta, int gamma, int var2, int var3, int var4, int var5)
